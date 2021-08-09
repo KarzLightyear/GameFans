a23da1ea4131a078b71fd7a0dd7e5eaccb8d6454
KarzLightyear <k.k.yamato@gmail.com>
2021-08-09 10:12:48 +0000

    stash

--- app/controllers/public/games_controller.rb
+++ app/controllers/public/games_controller.rb
@@ -1,2 +1,46 @@
 class Public::GamesController < ApplicationController
+  def index
+  end
+  
+  def list
+    @games = Game.all
+    @game = Game.new
+  end
+  
+  def create
+    @game = Game.new(game_params)
+    @game.customer_id = current_customer.id
+    if @game.save
+      redirect_to game_path(@game), notice: "You have created game successfully."
+    else
+      @games = Game.all
+      render 'list'
+    end
+  end
+  
+  def show
+  end
+  
+  def edit
+  end
+  
+  def update
+  end
+  
+  def destroy
+  end
+  
+  private
+
+  def game_params
+    params.require(:game).permit(:title, :body)
+  end
+
+  def ensure_correct_customer
+    @game = game.find(params[:id])
+    unless @game.customer == current_customer
+      redirect_to games_path
+    end
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,18 @@
 Rails.application.routes.draw do
-  
+
+  namespace :admin do
+    get 'homes/top'
+  end
   scope module: 'public' do
     root 'homes#top'
     get 'about', to: 'homes#about'
-    
+
     resources :games, only:[:index,:new,:create,:show,:edit,:update,:destroy]
     get 'list', to: 'games#list'
   end
-  
+
   namespace :admin do
+    root 'admin/sessions#new'
     resources :genres, only:[:index,:create,:edit,:update]
   end
 
