ade2d21165a3a27462088ac266977fbcd5eb98fd
KarzLightyear <k.k.yamato@gmail.com>
2021-08-11 14:24:02 +0000

    [edit]ヘッダー修正・会員ページ作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,5 @@
 Rails.application.routes.draw do
-
-  scope module: 'public' do
-    root 'homes#top'
-    get 'about', to: 'homes#about'
-
-    resources :games, only:[:index,:new,:create,:show,:edit,:update,:destroy]
-    get 'list', to: 'games#list'
-  end
-
-  namespace :admin do
-    root 'homes#top'
-    resources :genres, only:[:index,:create,:edit,:update,:destroy]
-  end
-
+  
   devise_for :admin, controllers: {
     sessions:      'admin/sessions',
     passwords:     'admin/passwords',
@@ -24,4 +11,20 @@ Rails.application.routes.draw do
     passwords:     'customers/passwords',
     registrations: 'customers/registrations'
   }
+
+  scope module: 'public' do
+    root 'homes#top'
+    get 'about', to: 'homes#about'
+
+    resources :games, only:[:index,:new,:create,:show,:edit,:update,:destroy]
+    resources :customers, only:[:index,:show,:edit,:update]
+    get 'list', to: 'games#list'
+  end
+
+  namespace :admin do
+    root 'homes#top'
+    resources :genres, only:[:index,:create,:edit,:update,:destroy]
+  end
+
+  
 end
